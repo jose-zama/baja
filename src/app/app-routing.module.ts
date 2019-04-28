64fc163565ed3c088f1522f0e2eec3d2e29df061
Zamudio, Jose <jose.zamudio@thermofisher.com>
2019-04-28 11:29:33 -0700

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-const routes: Routes = [];
+import {BrowserComponent}      from './browser/browser.component';
+import {CooperateFormComponent}      from './cooperate-form/cooperate-form.component';
+
+const routes: Routes = [
+  {path: 'investment', component: BrowserComponent},
+  {path: 'invest/detail/:id', component: CooperateFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDividerModule,MatCardModule,MatButtonModule,MatListModule,MatToolbarModule, MatSidenavModule, MatIconModule, MatGridListModule, MatMenuModule, MatInputModule, MatSelectModule, MatRadioModule} from '@angular/material';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { BrowserComponent } from './browser/browser.component';
+import { CooperateFormComponent } from './cooperate-form/cooperate-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BrowserComponent,
+    CooperateFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule,
+    LayoutModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
