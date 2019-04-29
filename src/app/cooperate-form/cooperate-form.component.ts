21a52288c6a800e68d5ae215c4deeacab2c66ba5
Luis Brito <ing.l.brito@gmail.com>
2019-04-28 17:13:07 -0700

    Endend investment thingy

--- src/app/cooperate-form/cooperate-form.component.ts
+++ src/app/cooperate-form/cooperate-form.component.ts
@@ -16,11 +16,13 @@ export class CooperateFormComponent implements OnInit {
   selectedId: number;
   alreadyInvestedPercent: number;
   investment: number;
+  isPayed: boolean = false;
 
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
+
 
   constructor(
     private _formBuilder: FormBuilder,
@@ -32,14 +34,20 @@ export class CooperateFormComponent implements OnInit {
     this.equipment = equipmentList.find( x => x.id == this.selectedId);
     this.alreadyInvestedPercent = (this.equipment.currentInvestment * 100) / this.equipment.price;
 
+
     this.firstFormGroup = this._formBuilder.group({
     });
 
     this.secondFormGroup = this._formBuilder.group({
-      investment: [0, [Validators.min(1), Validators.max(this.equipment.price - this.equipment.currentInvestment)]]
+      investment: [1, [Validators.min(1), Validators.max(this.equipment.price - this.equipment.currentInvestment)]]
     });
 
     this.thirdFormGroup = this._formBuilder.group({
     });
+  }
+
+  pay(){
+    console.warn("PAYMENT!")
+    this.isPayed = true;
   }
 }
